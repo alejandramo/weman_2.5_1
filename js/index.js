d045356af9cdc52dd7065fe650e3e16d3c259acb
alejandramo <jalejandra.momari@gmail.com>
2019-05-02 11:38:57 -0500

    se movió carpeta a repositorio

--- js/index.js
+++ js/index.js
@@ -15,7 +15,25 @@ function ordenaPorHabitacion(arreglo) {
 	//En esta función escribe la función que ordenará el arreglo por habitación
 }
 
-function ordenaPorHuesped(arreglo) {
+function ordenaPorHuesped(registro) {
+	//1. Llamar a la variable registro
+	//2. Hacer sort de la variable 
+	var registro = [
+		{numHabitacion: "87", nombreHuesped: "Donna Noble"},
+		{numHabitacion: "34", nombreHuesped: "Jean Luc Piccard"},
+		{numHabitacion: "23", nombreHuesped: "Deana Troi"},
+		{numHabitacion: "12", nombreHuesped: "Christpher Pike"},
+		{numHabitacion: "67", nombreHuesped: "Amy Pond"},
+		{numHabitacion: "45", nombreHuesped: "Miyamoto Musashi"},
+		{numHabitacion: "93", nombreHuesped: "Amelia Earhart"},
+		{numHabitacion: "47", nombreHuesped: "Katherin Janeway"}
+	];
+	function ordenarAsc(p_array_json, p_key) {
+		p_array_json.sort(function (a, b) {
+		   return a[p_key] > b[p_key];
+		});
+	 }
 	//Acá puedes hacer la otra que ordene por el nombre del huesped
+
 }
 
